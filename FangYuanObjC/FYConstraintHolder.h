a9d96ed4fcb3372faf6775d85285de455687ccaf
王策 <634692517@qq.com>
2016-05-11 11:28:53 +0800

    add constraint holder

---
+++ FangYuanObjC/FYConstraintHolder.h
@@ -0,0 +1,24 @@
+//
+// Created by 王策 on 16/5/11.
+//
+
+
+#import <Foundation/Foundation.h>
+#import "FYConstraint.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FYConstraintHolder : NSObject
+
+@property (nonatomic, strong) FYConstraint * _Nullable topBottom;
+@property (nonatomic, strong) FYConstraint * _Nullable bottomTop;
+@property (nonatomic, strong) FYConstraint * _Nullable leftRight;
+@property (nonatomic, strong) FYConstraint * _Nullable rightLeft;
+
+- (FYConstraint * _Nullable)constraintAt:(FYConstraintDirection)direction;
+
+- (void)set:(FYConstraint * _Nullable)constraint At:(FYConstraintDirection)direction;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FangYuanObjC/FYConstraintHolder.m
@@ -0,0 +1,11 @@
+//
+// Created by 王策 on 16/5/11.
+//
+
+#import "FYConstraintHolder.h"
+
+
+@implementation FYConstraintHolder {
+
+}
+@end
